b920c55b7ecd9cbbdb8f488865a811df9f96bced
“Samara <samara.limads@gmail.com>
2023-12-06 22:09:50 +0100

    labs 1 day 3

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
+let tongueTwister = s1 + " " + s2 + " " + s3 + " " + s4 + " " + s5;
 
+let reverse = " " + s3 + " " + s2 + " " + s1 + " " + s4;
 
 // Print out the concatenated string
-
-
-
+console.log(tongueTwister + reverse);
 
 /*******************************************
     Iteration 1.2 | Camel Tail
@@ -23,11 +23,13 @@ const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
 
+const char1 = part1[3].toUpperCase();
+const char2 = part2[5].toUpperCase();
+const strpart1 = part1.slice(0, 3);
+const strpart2 = part2.slice(0, 5);
 
 // Print the cameLtaiL-formatted string
-
-
-
+console.log(strpart1 + char1 + strpart2 + char2);
 
 /*******************************************
     Iteration 2.1 | Calculate Tip
@@ -35,23 +37,23 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
+const billTotal = 84;
 
+let tipAmount = billTotal * 0.15;
 
 // Print out the tipAmount
 
-
-
+console.log(tipAmount);
 
 /*******************************************
     Iteration 2.2 | Generate Random Number
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
+const randomNumber = Math.floorconsole.log(randomNumber);
 
 // Print the generated random number
-
-
+Math.random() * 10 + 1;
 
 /*******************************************
     Iteration 3.1 | Booleans
@@ -62,15 +64,22 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
+false;
 
 const expression2 = a || b;
+true;
 
 const expression3 = !a && b;
+false;
 
 const expression4 = !(a && b);
+true;
 
 const expression5 = !a || !b;
+true;
 
 const expression6 = !(a || b);
+false;
 
 const expression7 = a && a;
+true;
